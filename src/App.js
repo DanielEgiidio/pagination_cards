dbf0d1811cb6ebf4d8e5add5bc5372bbe4e14ee7
DanielEgidio <danielegidio@si.fiponline.edu.br>
2022-03-17 16:21:52 -0300

    Setup do react-paginate e estilização dos cards

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import React, { useState } from "react";
 import "./App.css";
-import JsonData from ".data.json";
+import JsonData from "./data.json";
+import ReactPaginate from "react-paginate";
 
 const App = () => {
-  const [users, setUsers] = useState(JsonData.slice(0, 20));
+  const [users, setUsers] = useState(JsonData.slice(0, 100));
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const usersPerPage = 10;
+  const pagesVisited = pageNumber * usersPerPage;
+
+  const displayUsers = users
+    .slice(pagesVisited, pagesVisited + usersPerPage)
+    .map((user) => {
+      return (
+        <div className="user">
+          <h3>{user.firstName}</h3>
+          <h3>{user.lastName}</h3>
+          <h3>{user.email}</h3>
+        </div>
+      );
+    });
+
+  const pageCount = Math.ceil(users.length / usersPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
 
   return (
     <div className="App">
-      {users.map((user) => {
-        return (
-          <div className="user">
-            <h3>{user.firstName}</h3>
-            <h3>{user.lastName}</h3>
-            <h3>{user.email}</h3>
-          </div>
-        );
-      })}
+      {displayUsers}
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={"paginationBttns"}
+        previousLinkClassName={"previousBttn"}
+        nextLinkClassName={"nextBttn"}
+        disabledClassName={"paginationDisabled"}
+        activeClassName={"paginationActive"}
+      />
     </div>
   );
 };
